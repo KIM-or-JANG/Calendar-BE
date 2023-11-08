25f9f4e36b67e4ea7d729064d7d383438a02f49f
Jangkiwoong <zoo11111@gmail.com>
2023-11-09 00:25:26 +0900

    add security

---
+++ src/main/java/com/example/calendar/repository/ScheduleRepository.java
@@ -0,0 +1,10 @@
+package com.example.calendar;
+
+import com.example.calendar.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByroom_Id();
+}
