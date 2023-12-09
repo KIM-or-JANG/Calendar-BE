d73b10c6b544e2016612fd470f213191edf83971
Jangkiwoong <wkdrldnd99@gmail.com>
2023-12-10 01:30:53 +0900

    cicd

--- src/main/java/com/example/calendar/asdController.java
+++ src/main/java/com/example/calendar/asdController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 @RequiredArgsConstructor
 public class asdController {
-
-//    @GetMapping("/asd")
-//    public ResponseEntity<Message> asd ()
-//             {
-//        return new ResponseEntity<>(new Message("테스트 성공",null), HttpStatus.OK);
-//    }
+//      15.165.160.195
+    @GetMapping("/asd")
+    public ResponseEntity<Message> asd ()
+             {
+        return new ResponseEntity<>(new Message("테스트 성공",null), HttpStatus.OK);
+    }
 }
