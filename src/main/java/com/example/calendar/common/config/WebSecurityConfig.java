14faa3ab4fc4cd00b2852021b9270a6e7386759f
Jangkiwoong <wkdrldnd99@gmail.com>
2023-12-10 16:43:27 +0900

    connect db

--- src/main/java/com/example/calendar/common/config/WebSecurityConfig.java
+++ src/main/java/com/example/calendar/common/config/WebSecurityConfig.java
@@ -45,14 +45,14 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         return configuration.getAuthenticationManager();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // resources 접근 허용 설정
-        return web -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())  // H2 > MySQL 전환시 삭제
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-
-    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        // resources 접근 허용 설정
+//        return web -> web.ignoring()
+//                .requestMatchers(PathRequest.toH2Console())  // H2 > MySQL 전환시 삭제
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
