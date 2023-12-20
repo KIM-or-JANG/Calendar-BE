979e7dc09ac1f858a5fbae4f6642a1af2f277717
Jangkiwoong <wkdrldnd99@gmail.com>
2023-12-20 12:34:40 +0900

    test

--- src/main/java/com/example/calendar/asdController.java
+++ src/main/java/com/example/calendar/asdController.java
@@ -1,20 +1,20 @@
-package com.example.calendar;
-
-import com.example.calendar.common.util.Message;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping
-@RequiredArgsConstructor
-public class asdController {
-    @GetMapping("/asd")
-    public ResponseEntity<Message> asd ()
-             {
-        return new ResponseEntity<>(new Message("테스트 성공",null), HttpStatus.OK);
-    }
-}
+//package com.example.calendar;
+//
+//import com.example.calendar.common.util.Message;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping
+//@RequiredArgsConstructor
+//public class asdController {
+//    @GetMapping("/asd")
+//    public ResponseEntity<Message> asd ()
+//             {
+//        return new ResponseEntity<>(new Message("테스트 성공",null), HttpStatus.OK);
+//    }
+//}
